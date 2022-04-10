6c4677c1cd34ee8e32fb0b261949a2fff0d0c392
caitlindw <caitlindwalker@wustl.edu>
2022-04-09 19:10:32 -0500

    Updates from Thursday!
    Wrote out comments for iteration plan :)

--- mems5701proj.m
+++ mems5701proj.m
@@ -3,7 +3,8 @@ Tt_p = 540; %Rankine
 Pt_chamber = 2*10^5; %psf
 Cfg = 0.96;
 gamma = 1.4;
-R0_air = 49710/(28.97*0.06852177); %gas constant / mixture avg molecular weight (slug/Kmol)
+R0 = 49710/(28.97*0.06852177); %gas constant / mixture avg molecular weight (slug/Kmol)
+C_P0 = ((gamma)/(gamma-1))*R0; %lbf/slug-R
 
 %TO DO
 % compute R, C_p for all species
@@ -17,7 +18,7 @@ T0_launch   = 411.84;       % R - Atmosphere Table
 P0_launch   = 4.373 * 144;  % lbf / sq_ft
 
 F_np_min_launch = 4000; %lbf
-V0_launch = M0_launch*sqrt(gamma*R0_air*T0_launch);
+V0_launch = M0_launch*sqrt(gamma*R0*T0_launch);
 
 
 %cowl drag, launch
@@ -47,7 +48,7 @@ P0_cruise   = 1.049 * 144;  % lbf / sq_ft
 
 F_np_min_cruise = 5000; %lbf
 
-V0_cruise = M0_cruise*sqrt(gamma*R0_air*T0_cruise);
+V0_cruise = M0_cruise*sqrt(gamma*R0*T0_cruise);
 
 %cowl drag cruise
 C_D_cruise = 0.4;
@@ -61,4 +62,34 @@ D_cowl_cruise = q_cruise*C_D_cruise*A_cowl_cruise;
 M15_cruise = 0.2;
 
 %section 7 -> 9 isentropic flow, mixer
+
+%select a0 using 2 nested for-loops
+%equivalence = outer loop
+    
+
+    %area = inner loop
+        %compute m_dot0 = m_dot15 from A0
+        %mdot_0 = V0*rho0*A0
+        
+        %find Pt15 from flight condition + recovery
+            %can get Pt0
+            %get Pt15 = Pt2 = Pt0*pi_inlet
+            %M15 = 0.2
+            %could find Tt15, then A15
+            
+            %innermost loop: guess m_dotc = 200lb/s
+                %stop condition = F_N given -> make a while loop
+                %for selected ph and beta = 0, compute mass flows of H, O
+                    %Mk_O2 = 32
+                    %Mk_H2 = 4
+                    %phi = (m_dotH2/m_dotO2)/(mH2_st/mO2_st)
+                    %mH2_st = 2*2;
+                    %mO2_st = 1*32;
+                    %syms m_dotH2 m_dotO2
+                    %eq1 = phi == (m_dotH2/m_dotO2)/(mH2_st/mO2_st);
+                    %eq2 = m_dotc = m_dotH2 + m_dotO2;
+                    %[m_dotH2_sol,m_dotO2+sol] = solve([eq1,eq2],[m_dotH2,m_dotO2])
+                    
+    
+
 
